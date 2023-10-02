c8d26b31ac53c496e2ca5de6c31b0ea926f2b4ad
sgautier-dev <sgautier.dev@gmail.com>
2023-10-02 17:24:46 +0400

    started to implement cloud firestore

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: ["tsconfig.json", "tsconfig.dev.json"],
+    tsconfigRootDir: __dirname,
     sourceType: "module",
   },
   ignorePatterns: [
@@ -27,6 +28,9 @@ module.exports = {
   rules: {
     "quotes": ["error", "double"],
     "import/no-unresolved": 0,
-    "indent": ["error", 2],
+    "indent": "off",
+    "no-tabs": 0,
+    "object-curly-spacing": [2, "always"],
+    "@typescript-eslint/no-var-requires": 0,
   },
 };
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,13 +7,18 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {onRequest} from "firebase-functions/v2/https";
+import { onRequest } from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const onScraperComplete = onRequest((request, response) => {
+	console.log("SCRAPE COMPLETE: ", request.body);
+	logger.info("Hello logs!", { structuredData: true });
+	response.send("Hello from Firebase!");
+});
+
+// creating a ngrok tunnel
+// on the terminal: ngrok http 5001
+// https://f27b-165-169-98-136.ngrok-free.app/data-scraper-8a73c/us-central1/onScraperComplete
